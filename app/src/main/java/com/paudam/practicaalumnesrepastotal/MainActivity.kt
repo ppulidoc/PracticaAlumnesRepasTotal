19ac1f491208afa43c88bb44f9dbfeba02c91b90
paupc <p.pulido.c@institutvidreres.cat>
2025-01-16 09:43:16 +0100

    add bottom menu

--- app/src/main/java/com/paudam/practicaalumnesrepastotal/MainActivity.kt
+++ app/src/main/java/com/paudam/practicaalumnesrepastotal/MainActivity.kt
@@ -5,13 +5,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.paudam.practicaalumnesrepastotal.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding:ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        enableEdgeToEdge()
 
-        setContentView(R.layout.activity_main)
-        //setupActionBarWithNavController(findNavController(R.id.myNavHostFragment))
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupWithNavController(binding.bottomNavigationView, navController)
 
     }
 }
